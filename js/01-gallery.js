141637c4a6bbcf48df890fc48c67ad0a2ce8d60b
Domin737 <p.dominiak.pd@gmail.com>
2024-01-24 02:04:35 +0100

    add task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
+const galleryContainer = document.querySelector(".gallery");
+
+const createGalleryItemsMarkup = (items) => {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>
+      `;
+    })
+    .join("");
+};
+
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.innerHTML = galleryItemsMarkup;
+
+galleryContainer.addEventListener("click", onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const sourceUrl = event.target.dataset.source;
+  const instance = basicLightbox.create(`
+      <img src="${sourceUrl}" width="800" height="600">
+  `);
+  instance.show();
+}
 console.log(galleryItems);
